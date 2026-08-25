a751bdaa9d8fd72a9e549080eb0da5d77e2af229
eyezah (Gigabox) <50851883+eyezahhhh@users.noreply.github.com>
2026-08-26 08:34:32 +1200

    support relative NEXT_PUBLIC_API_URL

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,10 @@ if (typeof window === "undefined") {
 	const serverUrl = process.env.INTERNAL_API_URL || "http://127.0.0.1:3000";
 	setBaseUrl(serverUrl);
 } else {
-	const clientUrl = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:3000";
+	let clientUrl = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:3000";
+	if (clientUrl.startsWith("/")) {
+		clientUrl = window.location.origin + clientUrl;
+	}
 	setBaseUrl(clientUrl);
 }
 
