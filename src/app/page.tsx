a8bb0ecb4ab5a00a7e78ab0c0524d323386f9da6
eyezah (Gigabox) <50851883+eyezahhhh@users.noreply.github.com>
2026-09-03 15:53:58 +1200

    Add links to wiki and Discord

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,22 @@
 
 import { RequireAuth } from "@/guard/auth.guard";
 import styles from "./page.module.scss";
+import Link from "next/link";
 
 export default function Home() {
 	return (
 		<RequireAuth>
-			<h1 className={styles.title}>Welcome to Pipe Bomb</h1>
+			<div className={styles.container}>
+				<h1 className={styles.title}>Welcome to Pipe Bomb</h1>
+				<div className={styles.links}>
+					<Link href="https://pipebomb.net/wiki" target="_blank">
+						Wiki
+					</Link>
+					<Link href="https://discord.gg/Kj7KfPDwCE" target="_blank">
+						Discord
+					</Link>
+				</div>
+			</div>
 		</RequireAuth>
 	);
 }
